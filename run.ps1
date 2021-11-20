156a2448d6256c8086898f6f697517598585b58d
Vlad Pirlog <avi.pirlog@gmail.com>
2021-11-20 17:42:08 +0200

    Refactor modules of the app
    Refactor configuration files and setup scripts
    
    Replace conf.json with dotenv files
    
    Add CI pipeline for linting and running the tests
    
    Update requirements.txt and .gitignore
    
    Add configuration file for autopep8

--- run.ps1
+++ run.ps1
@@ -1,2 +1,5 @@
 $env:FLASK_APP = ".\src\main.py"
+$env:FLASK_ENV = "development"
+$env:FLASK_RUN_HOST = "127.0.0.1"
+$env:FLASK_RUN_PORT = "9999"
 python3 -m flask run
--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-FLASK_APP="./src/main.py" python3 -m flask run 
+export FLASK_APP=./src/main.py
+export FLASK_ENV=development
+export FLASK_RUN_HOST=127.0.0.1
+export FLASK_RUN_PORT=8888
+python3 -m flask run
