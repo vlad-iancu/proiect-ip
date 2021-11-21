187d373dd5a49818cdffcd6ad8185b0c69e5d302
Iancu Vlad <iancuvladalexandru@gmail.com>
2021-11-21 13:41:25 +0200

    Functional schema file
    Add mock data db insert statements and use caps conventions for sql data types

--- schema.sql
+++ schema.sql
@@ -1,40 +1,36 @@
-CREATE TABLE `Ingredient` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `measure_unit` varchar(255) NOT NULL,
-  `available` float4 NOT NULL
+CREATE TABLE Ingredient (
+  id INTEGEREGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255) NOT NULL,
+  measure_unit VARCHAR(255) NOT NULL,
+  available DECIMAL(10,5) NOT NULL
 );
 
-CREATE TABLE `CoffeeRecipe` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL
+CREATE TABLE CoffeeRecipe (
+  id INTEGEREGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `CoffeeRecipeIngredient` (
-  `recipe_id` int,
-  `ingredient_id` int,
-  `quantity` float4 NOT NULL,
-  PRIMARY KEY (`recipe_id`, `ingredient_id`)
+CREATE TABLE CoffeeRecipeIngredient (
+  recipe_id INTEGER,
+  ingredient_id INTEGER,
+  quantity DECIMAL(10,5) NOT NULL,
+  PRIMARY KEY (recipe_id, ingredient_id),
+  FOREIGN KEY (recipe_id) REFERENCES CoffeeRecipe (id),
+  FOREIGN KEY (ingredient_id) REFERENCES Ingredient (id)
 );
 
-CREATE TABLE `CoffeePreparation` (
-  `recipe_id` int,
-  `started_at` timestamp,
-  `finished_at` timestamp,
-  `ingredients_json` varchar(255) NOT NULL,
-  PRIMARY KEY (`recipe_id`, `started_at`)
+CREATE TABLE CoffeePreparation (
+  recipe_id INTEGER,
+  started_at TIMESTAMP,
+  finished_at TIMESTAMP,
+  ingredients_json VARCHAR(255) NOT NULL,
+  PRIMARY KEY (recipe_id, started_at),
+  FOREIGN KEY (recipe_id) REFERENCES CoffeeRecipe (id)
 );
 
-CREATE TABLE `Log` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `type` varchar(255) NOT NULL,
-  `properties_json` varchar(255) NOT NULL,
-  `at` timestamp NOT NULL
+CREATE TABLE Log (
+  id INTEGEREGER PRIMARY KEY AUTOINCREMENT,
+  type VARCHAR(255) NOT NULL,
+  properties_json VARCHAR(255) NOT NULL,
+  at TIMESTAMP NOT NULL
 );
-
-ALTER TABLE `CoffeeRecipeIngredient` ADD FOREIGN KEY (`recipe_id`) REFERENCES `CoffeeRecipe` (`id`);
-
-ALTER TABLE `CoffeeRecipeIngredient` ADD FOREIGN KEY (`ingredient_id`) REFERENCES `Ingredient` (`id`);
-
-ALTER TABLE `CoffeePreparation` ADD FOREIGN KEY (`recipe_id`) REFERENCES `CoffeeRecipe` (`id`);
-
