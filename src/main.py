bb582f3a331023dd24675b70dadfe51b0aa746b5
Vlad Pirlog <avi.pirlog@gmail.com>
2022-02-01 20:29:23 +0200

    Fix / Add auth secret key and fix user repo

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from src.config.Configuration import get_configuration
 import src.db as db
 import src.controllers.AuthController as auth
 import src.controllers.CoffeePreparationController as coffeepreparation
@@ -7,6 +8,7 @@ import src.controllers.CoffeeRecipeController as coffeerecipe
 import src.controllers.IngredientController as ingredient
 
 app = Flask(__name__)
+app.config.from_mapping(SECRET_KEY=get_configuration().auth_secret)
 
 app.register_blueprint(auth.bp)
 app.register_blueprint(coffeerecipe.bp)
